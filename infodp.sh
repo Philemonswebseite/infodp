2f0bdf4613ef05aba56b97bd6adcb9a3c293dba7
Philemonswebseite <150483123+Philemonswebseite@users.noreply.github.com>
2025-04-25 22:43:39 +0200

    infodp test

--- infodp.sh
+++ infodp.sh
@@ -1,21 +1,5 @@
-#!/bin/bash
-
-# URLs der Websites
-URL1="https://example.com"
-URL2="https://example.org"
-
-# Warte, bis der Desktop-Manager vollständig geladen ist
-sleep 10
-
-# Chromium im Kiosk-Modus mit beiden Websites starten
-export DISPLAY=:0
-chromium-browser --new-window "$URL1" &
-sleep 2
-chromium-browser --new-window "$URL2" &
-
-# Warte kurz, damit beide Fenster geladen werden
-sleep 5
-
-# Fenster in Split View anordnen (erfordert wmctrl)
-wmctrl -r "$URL1" -e 0,0,0,960,1080
-wmctrl -r "$URL2" -e 0,960,0,960,1080
+ chromium-browser --kiosk infodp.de.cool/ver.php?ic=true &
+    sleep 5
+    wmctrl -r Chromium -b remove,fullscreen
+    wmctrl -r Chromium -b remove,maximized_vert,maximized_horz
+    wmctrl -r Chromium -e 0,0,0,3840,1080
