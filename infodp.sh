d2453f7c7c6f22de1e8b3b45ea875fe00fcbce84
Philemonswebseite <150483123+Philemonswebseite@users.noreply.github.com>
2025-04-25 22:45:06 +0200

    infodp update

--- infodp.sh
+++ infodp.sh
@@ -1,5 +1,20 @@
- chromium-browser --kiosk infodp.de.cool/ver.php?ic=true &
-    sleep 5
-    wmctrl -r Chromium -b remove,fullscreen
-    wmctrl -r Chromium -b remove,maximized_vert,maximized_horz
-    wmctrl -r Chromium -e 0,0,0,3840,1080
+#!/bin/bash
+
+# URLs der Websites
+URL1="https://infodp.de.cool/ver.php?ic=true"
+URL2="https://example.com"
+
+# Warte, bis der Desktop-Manager vollständig geladen ist
+sleep 5
+
+# Starte Chromium im App-Modus (ohne Rand) für beide Websites
+chromium-browser --app="$URL1" &
+sleep 2
+chromium-browser --app="$URL2" &
+
+# Warte kurz, damit beide Fenster geladen werden
+sleep 5
+
+# Fenster in Split-Screen-Ansicht anordnen
+wmctrl -r :ACTIVE: -e 0,0,0,1920,1080  # Linke Hälfte des Bildschirms
+wmctrl -r :SELECT: -e 0,1920,0,1920,1080  # Rechte Hälfte des Bildschirms
